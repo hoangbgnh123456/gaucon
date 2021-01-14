fc6e9883dd59d8b05cfc964fafb0a2a226aed08b
hoangbgnh123456 <72736756+hoangbgnh123456@users.noreply.github.com>
2021-01-14 21:02:46 +0700

    Update sidenav.php

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Hello, world!</title>
+  <title>Lipit Bear</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
@@ -25,8 +25,8 @@
       Tip 2: you can also add an image using data-image tag
   -->
       <div class="logo">
-        <a href="" class="simple-text logo-normal">
-          PuneethReddy
+        <a href="https://www.facebook.com/Smeb.Sama/" class="simple-text logo-normal">
+          GauCon
         </a>
       </div>
       <div class="sidebar-wrapper ps-container ps-theme-default" data-ps-id="3a8db1f4-24d8-4dbf-85c9-4f5215c1b29a">
