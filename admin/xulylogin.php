94bc202ed1dfcbae88e87a45d64dd318a5c0b96c
hoangbgnh123456 <72736756+hoangbgnh123456@users.noreply.github.com>
2021-01-19 21:32:30 +0700

    Update xulylogin.php

--- admin/xulylogin.php
+++ admin/xulylogin.php
@@ -3,8 +3,8 @@ $er='';
 $user='';
 session_start();
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    $name=$_POST["admin_name"];
-$pass=$_POST["admin_password"];
+    $name=$_POST["name"];
+$pass=$_POST["password"];
 $db=mysqli_connect("34.200.32.20","gaucon","123@123a","onlineshop");
 $sql="SELECT * FROM admin_info WHERE admin_name='{$name}' AND admin_password='{$pass}'";
 $rs=mysqli_query($db, $sql);
