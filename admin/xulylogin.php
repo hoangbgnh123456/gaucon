b54065ab7a37a27f55bce5378142a06eb49ef169
hoangbgnh123456 <72736756+hoangbgnh123456@users.noreply.github.com>
2021-01-19 20:43:03 +0700

    Add files via upload

---
+++ admin/xulylogin.php
@@ -0,0 +1,22 @@
+<?php
+$er='';
+$user='';
+session_start();
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $name=$_POST["name"];
+$pass=$_POST["password"];
+$db=mysqli_connect("localhost","root","","onlineshop");
+$sql="SELECT * FROM admin_info WHERE admin_name='{$name}' AND admin_password='{$pass}'";
+$rs=mysqli_query($db, $sql);
+$user = mysqli_fetch_assoc($rs);
+if(mysqli_num_rows($rs)){
+    $_SESSION['user'] = $user['admin_name'];
+    $user = $name;
+   header('location: index.php');
+}
+else{
+    $er="User name or password is incorrect";
+    include('Login.php');
+}
+}
+?>
