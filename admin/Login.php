09111446efd00b60e0d6e161b5aaa77f7f26c11e
hoangbgnh123456 <72736756+hoangbgnh123456@users.noreply.github.com>
2021-01-20 17:46:17 +0700

    Update Login.php

--- admin/Login.php
+++ admin/Login.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="loginn.css">
-    <script src="https://kit.fontawesome.com/4533631142.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <?php
